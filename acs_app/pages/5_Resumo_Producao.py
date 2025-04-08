d8cd0235612e32a49bfe445ce50a2d3adb5d445b
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:23:42 -0300

    Update and rename 5_Resumo_Producao.py to 5_Resumo_de_Produção.py

--- acs_app/pages/5_Resumo_Producao.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 5 Resumo Producao")
-st.write("Conteúdo de 5 Resumo Producao aqui.")
---
+++ acs_app/pages/5_Resumo_de_Produção.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 5 Resumo de Produção")
+st.write("Conteúdo de 5 Resumo de Produção aqui.")
