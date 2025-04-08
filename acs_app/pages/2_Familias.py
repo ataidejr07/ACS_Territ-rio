b6770a66ea04c700e860c02fc32204564e505286
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:19:26 -0300

    Update and rename 2_Familias.py to 2_Famílias.py

--- acs_app/pages/2_Familias.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 2 Familias")
-st.write("Conteúdo de 2 Familias aqui.")
---
+++ acs_app/pages/2_Famílias.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 2 Famílias")
+st.write("Conteúdo de 2 Famílias aqui.")
