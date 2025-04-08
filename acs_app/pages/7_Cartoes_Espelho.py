c995018a349a6b5971f1464d96579eb371a275b5
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:26:36 -0300

    Update and rename 7_Cartoes_Espelho.py to 7_Cartões_Espelho.py

--- acs_app/pages/7_Cartoes_Espelho.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 7 Cartoes Espelho")
-st.write("Conteúdo de 7 Cartoes Espelho aqui.")
---
+++ acs_app/pages/7_Cartões_Espelho.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 7 Cartões Espelho")
+st.write("Conteúdo de 7 Cartões Espelho aqui.")
