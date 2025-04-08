15764e1ea97aa2e80db9d82409ac7bca16adfc53
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:18:46 -0300

    Update and rename 1_Domicilios.py to 1_Domicílios.py

--- acs_app/pages/1_Domicilios.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 1 Domicilios")
-st.write("Conteúdo de 1 Domicilios aqui.")
---
+++ acs_app/pages/1_Domicílios.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 1 Domicílios")
+st.write("Conteúdo de 1 Domicílios aqui.")
