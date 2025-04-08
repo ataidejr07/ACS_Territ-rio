df94972a58258ac16612209809874407e8e8430a
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:19:56 -0300

    Update and rename 3_Cidadaos.py to 3_Cidadãos.py

--- acs_app/pages/3_Cidadaos.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 3 Cidadaos")
-st.write("Conteúdo de 3 Cidadaos aqui.")
---
+++ acs_app/pages/3_Cidadãos.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 3 Cidadãos")
+st.write("Conteúdo de 3 Cidadãos aqui.")
