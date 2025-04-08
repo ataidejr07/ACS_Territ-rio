77afd8e490254f947d13067d6eea3f1ab480ece7
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:24:50 -0300

    Update and rename 6_Nascimentos_Obitos.py to 6_Nascimentos_e_Óbitos.py

--- acs_app/pages/6_Nascimentos_Obitos.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 6 Nascimentos Obitos")
-st.write("Conteúdo de 6 Nascimentos Obitos aqui.")
---
+++ acs_app/pages/6_Nascimentos_e_Óbitos.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 6 Nascimentos e Óbitos")
+st.write("Conteúdo de 6 Nascimentos e Óbitos aqui.")
