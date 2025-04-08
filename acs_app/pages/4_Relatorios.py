508ce65816e85ea9ee130339d0aad8c145dbe6e1
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-04-08 10:20:38 -0300

    Update and rename 4_Relatorios.py to 4_Relatórios.py

--- acs_app/pages/4_Relatorios.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página: 4 Relatorios")
-st.write("Conteúdo de 4 Relatorios aqui.")
---
+++ acs_app/pages/4_Relatórios.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Página: 4 Relatórios")
+st.write("Conteúdo de 4 Relatórios aqui.")
